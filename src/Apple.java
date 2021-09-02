d3a71493b2573877dba025b16b7bded75b6638b0
Anthony <1249632746@qq.com>
2021-09-02 22:35:42 +0800

    commit

--- src/Apple.java
+++ src/Apple.java
@@ -4,6 +4,7 @@ public class Apple extends Fruit{
         super.num = 10;
         System.out.println("eat " + num + " Apple");
     }
+    // change this line
     public static void main(String[] args) {
         Fruit fruit = new Apple();
         fruit.eat();
